505f2095bf3328eb09c54cdecf1814d6ce0b7908
Sebastián Martínez <sebastian.martinez.serna@gmail.com>
2023-02-20 07:03:48 -0500

    feat: :sparkles: changes in education, it's ready

--- assets/js/education.js
+++ assets/js/education.js
@@ -8,63 +8,75 @@ const moocscards = [
     title: "Data Science",
     cardImage: "assets/images/education-page/coursera2.svg",
     moocLink: "https://www.coursera.org/browse/data-science",
+    mockPlatform: "Coursera",
   },
   {
     title: "Cryptography",
     cardImage: "assets/images/education-page/udemy.svg",
     moocLink: "https://www.udemy.com/course/cryptography-for-beginners/",
+  mockPlatform: "Coursera",
   },
   {
     title: "Machine Learning",
     cardImage: "assets/images/education-page/coursera2.svg",
     moocLink: "https://www.coursera.org/learn/machine-learning",
+    mockPlatform: "Coursera",
   },
   {
     title: "Introduction to HTML 5",
     cardImage: "assets/images/education-page/udacity.svg",
     moocLink: "https://www.udacity.com/course/intro-to-html-and-css--ud001",
+    mockPlatform: "Coursera",
   },
   {
     title: "Introduction to CSS 3",
     cardImage: "assets/images/education-page/udacity.svg",
     moocLink: "https://www.udacity.com/course/intro-to-html-and-css--ud001",
+    mockPlatform: "Coursera",
   },
   {
     title: "Javascript",
     cardImage: "assets/images/education-page/udacity.svg",
     moocLink: "https://www.udacity.com/course/intro-to-javascript--ud803",
+    mockPlatform: "Coursera",
   },
   {
     title: "Bootstrap 4",
     cardImage: "assets/images/education-page/udemy.svg",
     moocLink: "https://www.udemy.com/course/bootstrap-4-tutorials/",
+    mockPlatform: "Coursera",
   },
   {
     title: "Intro to React",
     cardImage: "assets/images/education-page/edx.svg",
     moocLink: "https://www.edx.org/learn/reactjs",
+    mockPlatform: "Coursera",
   },
   {
     title: "Intro to React Native",
     cardImage: "assets/images/education-page/coursera2.svg",
     moocLink:
       "https://www.coursera.org/lecture/react-native/introduction-to-react-native-Eax0D",
+    mockPlatform: "Coursera",
   },
   {
     title: "NodeJS, Express and MongoDB",
     cardImage: "assets/images/education-page/coursera2.svg",
     moocLink: "https://www.coursera.org/learn/server-side-nodejs",
+    mockPlatform: "Coursera",
   },
   {
     title: "XML-AJAX",
     cardImage: "assets/images/education-page/udemy.svg",
     moocLink: "https://www.udemy.com/course/xml-from-beginner-to-expert/",
+    mockPlatform: "Coursera",
   },
   {
     title: "Data Structures & Algorithms",
     cardImage: "assets/images/education-page/udacity.svg",
     moocLink:
       "https://www.udacity.com/course/data-structures-and-algorithms-nanodegree--nd256",
+    mockPlatform: "Coursera",
   },
 ];
 
@@ -111,25 +123,18 @@ function showExperience() {
 const showCards = () => {
   let output = "";
   moocscards.forEach(
-    ({ title, cardImage, moocLink }) =>
-      (output += `        
-        <div class="col-6 col-md-3 col-sm-4 column" data-aos="fade-up" data-aos-easing="linear" data-aos-delay="600" >  
-            <div class="card mb-3 mx-auto">
-               <div class="content">
-                  <div class="content-overlay"></div>
-                    <img src=${cardImage} class="card-img-top content-image">     
-                  <div class="content-details fadeIn-bottom">
-                    <a href="${moocLink}" target="_blank"><i class="fa fa-info-circle fa-2x" aria-hidden="true" style="color: white;"></i></a>                                   
-                  </div>
-                </div>
-                <div class="card-body">
-                    <h6 class="mt-0 py-2 text-center font-weight-bold mooc-title" style="font-size:12px;">${title}</h6>
-                </div>
-            </div>
-        </div>        
+    ({ title, moocLink, mockPlatform}) =>
+    (output += `
+        <div class="col-6 column">
+          <i class="fa-solid fa-chevron-right icon color-primary"></i>
+          <span class="color-secondary fw-bold">${mockPlatform} : </span>
+          <a href="${moocLink}" class="link-neon" target="_blank">
+            ${title}
+          </a>
+        </div>
       `)
   );
-  moocs.innerHTML = output;
+  // moocs.innerHTML = output;
 };
 document.addEventListener("DOMContentLoaded", showCards);
 
@@ -158,8 +163,8 @@ const showCards1 = () => {
   let output = "";
   badgesection.forEach(
     ({ title, image, description }) =>
-      (output += `       
-      <div class="col-lg-4 col-md-6 p-2" data-aos="fade-up" data-aos-easing="linear" data-aos-delay="600"> 
+      (output += `
+      <div class="col-lg-4 col-md-6 p-2" data-aos="fade-up" data-aos-easing="linear" data-aos-delay="600">
         <img class="img-fluid d-block mb-3 mx-auto hvr-grow" src="${image}" alt="Card image cap" width="200">
           <div class="text-center font-weight-bolder" style="font-size: 1.3em;">${title}</div>
           <div class="text-center text-muted font-weight-bolder p-2">${description}</div>
@@ -174,12 +179,13 @@ document.addEventListener("DOMContentLoaded", showCards1);
 $(function () {
   window.sr = ScrollReveal();
 
-  if ($(window).width() < 768) {
+  if ($(window).width() < 850) {
     if ($(".timeline-content").hasClass("js--fadeInLeft")) {
       $(".timeline-content")
         .removeClass("js--fadeInLeft")
         .addClass("js--fadeInRight");
     }
+    $(".timeline-content").removeClass("timeline-item-bg");
 
     sr.reveal(".js--fadeInRight", {
       origin: "right",
@@ -203,17 +209,4 @@ $(function () {
     });
   }
 
-  sr.reveal(".js--fadeInLeft", {
-    origin: "left",
-    distance: "300px",
-    easing: "ease-in-out",
-    duration: 800,
-  });
-
-  sr.reveal(".js--fadeInRight", {
-    origin: "right",
-    distance: "300px",
-    easing: "ease-in-out",
-    duration: 800,
-  });
 });
