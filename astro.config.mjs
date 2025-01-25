fa48193be614c0dd9339a8467397f93d7fea2e4f
Sebastián Martínez <sebastian.martinez.serna@gmail.com>
2025-01-25 13:10:51 -0500

    feat: add sitemap to config

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,9 +2,11 @@
 import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 
+import sitemap from '@astrojs/sitemap';
+
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()],
+  integrations: [tailwind(), sitemap()],
   site: 'https://juans3.github.io',
   base: '/',
   build: {
